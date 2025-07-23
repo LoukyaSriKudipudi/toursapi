b38331da3493c0097c90fdb4fd93096ba26f6d54
LoukyaSriKudipudi <loukyasrikudipudi@gmail.com>
2025-07-23 23:20:47 +0530

    Added CORS support and updated API routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const tourRoutes = require('./routes/tourRoutes');
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tours', tourRoutes);
 
